b74e9ba01f6a2650b863c247ef52c00ab2c6dba2
Admin <sneha.jadhav2402@gmail.com>
2021-07-23 13:48:04 +0530

    Initial commit

---
+++ .idea/main.dart
@@ -0,0 +1,22 @@
+
+main(List<String> arguments){ //entry point for execution
+
+  //Variable= spot in memory. or just a bucket to hold value.value that can vary
+  /*Type??
+      String
+      Numbers
+      booleand
+      lists
+      maps
+  ..*/
+
+  /*var is generic variable type*/
+  var country='India'; //string
+  country=1; //int
+  String name;
+  name="Sneha";
+  print(name);
+
+
+  num
+}
